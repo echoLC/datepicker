69c018118c2ca5e9240360d1eca8e74242ac7ce3
luochao <luochao@yy.com>
2019-11-01 22:40:59 +0800

    feat: add basic example

---
+++ src/components/Examples/Basic.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="basic">
+    <datepicker v-model="date">
+
+    <button @click="getCurrentDate"></button>  
+  </div>
+</template>
+
+<script>
+import Datepicker from '../Datepicker/datepicker'
+
+export default {
+  name: 'Basic',
+
+  components: {
+    Datepicker
+  },
+
+  data () {
+    return {
+      date: new Date()
+    }
+  },
+
+  getCurrentDate () {
+    console.log(this.date)
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.basic {
+  margin: 0;
+  padding: 20px;
+}
+</style>
